8ccae86b7ce652968849a14098ffcf535448d01c
imosudi <imosudi@gmail.com>
2017-08-17 15:52:40 +0100

    Thu Aug 17 15:52:40 WAT 2017

--- weather.py
+++ weather.py
@@ -1,27 +1,28 @@
 filename = "port-harcourt-weather.txt"
 
-with open(filename) as file:
-	dayList = []
-	dailyTempSpread = []
-	file.next()
-	file.next()
-	for line in file:
-		line.strip()
-		splitted_line = line.split()
-		try:
-			dayListNum = int(splitted_line[0])
-			dailyHigh = int(splitted_line[1])
-			dailyLow = int(splitted_line[2])
-			#print int(splitted_line[0])
-		except ValueError:
-			pass
-		dailyTempSpread.append(dailyHigh - dailyLow)
-		dayList.append(dayListNum)
-	#print len(dayList)
-	#print len(dailyTempSpread)
+def weatherReport():
+	with open(filename) as file:
+		dayList = []
+		dailyTempSpread = []
+		file.next()
+		file.next()
+		for line in file:
+			line.strip()
+			splitted_line = line.split()
+			try:
+				dayListNum = int(splitted_line[0])
+				dailyHigh = int(splitted_line[1])
+				dailyLow = int(splitted_line[2])
+			except ValueError:
+				pass
+			dailyTempSpread.append(dailyHigh - dailyLow)
+			dayList.append(dayListNum)
 
-weatherDict = dict(zip(dayList, dailyTempSpread))
-print weatherDict
+	weatherDict = dict(zip(dayList, dailyTempSpread))
+	print weatherDict
+	print min(sorted(weatherDict, values=weatherDict.get))
+	return dayList, dailyTempSpread, weatherDict
 
 
+weatherReport()
 	
